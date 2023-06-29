aeab655b7d8bf34d50ff542e19922c81c80fe014
Sameer Mahajan <sameer.s.mahajan@gmail.com>
2023-06-29 17:24:00 +0530

    Update inference.py

--- tensorflow/inference.py
+++ tensorflow/inference.py
@@ -2,6 +2,18 @@ import tensorflow as tf
 from keras import models
 import numpy as np
 
+def get_spectrogram(waveform):
+  # Convert the waveform to a spectrogram via a STFT.
+  spectrogram = tf.signal.stft(
+      waveform, frame_length=255, frame_step=128)
+  # Obtain the magnitude of the STFT.
+  spectrogram = tf.abs(spectrogram)
+  # Add a `channels` dimension, so that the spectrogram can be used
+  # as image-like input data with convolution layers (which expect
+  # shape (`batch_size`, `height`, `width`, `channels`).
+  spectrogram = spectrogram[..., tf.newaxis]
+  return spectrogram
+  
 model = tf.keras.models.load_model("marathi-20")
 model.summary()
 
